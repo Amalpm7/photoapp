8c376fb312640151701b0d161455107a220ff8f0
Amalpm7 <amal.p@nestgroup.net>
2022-10-12 12:48:04 +0530

    service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { PhotosComponent } from './photos/photos.component';
-
+import { HttpClientModule } from "@angular/common/http";
 const photos:Routes=[
   {
     path:"",component:PhotosComponent
@@ -21,7 +21,8 @@ const photos:Routes=[
   imports: [
     BrowserModule,
     AppRoutingModule,
-    RouterModule.forRoot(photos)
+    RouterModule.forRoot(photos),
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
